82b8493a7dfcf86be0b4429471ee5355100b4bf0
Liyaan Khoso <115876366+blackstargunk@users.noreply.github.com>
2023-05-22 22:38:56 +0200

    Update and rename hello.py to app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+from db import get_records_by_year_and_bech_rating
 
 # after importing the modules, start writing the functions. When a function is wrapped in @app.route,
 # it is the code for a webpage. All the html documents must be in the 'templates' folder to be rendered.
@@ -6,15 +7,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 # homepage route
 @app.route('/')
 def homepage():
     return render_template('index.html')
 
+
 # search bar route
 @app.route('/search')
 def search():
-    return render_template('search.html')
+    return render_template('search_radio.html')
+
 
 # account login route
 @app.route('/savedplaylists', methods=["POST", "GET"])
@@ -22,15 +26,27 @@ def playlists():
     name = request.form["username"]
     return render_template("savedplaylists.html", username=name)
 
+
 # search results route
 @app.route('/results', methods=["POST", "GET"])
 def results():
-    start_year = int(request.form["decade"])
-    end_year = int(start_year) + 10
-    bechdel_test_score = request.form["bechdel"]
-    main_character_value = request.form["Main Character"]
-    return render_template('results.html', year=f"{start_year}-{end_year}", score=bechdel_test_score,
-                           main_character_value=main_character_value)
+    start_year = request.form.get('start_year')
+    end_year = request.form.get('end_year')
+    bechdel_rating = request.form.get('bechdel_rating')
+
+    results = get_records_by_year_and_bech_rating(start_year, end_year, bechdel_rating)
+    return render_template('results.html', results=results)
+
+# about page route
+@app.route('/about')
+def about():
+    pass
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=8000, debug=True)
+
+# to run the application via terminal and enter, in this order:
+# FLASK_APP=app.py
+# export FLASK_DEBUG=1
+# flask run
 
